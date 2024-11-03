759282733ba1d9d998756b4e380194958bb049f9
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2024-11-02 23:43:11 -0300

    Primeira Atualização

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# PSET_LanguageProg
-PSET referente a matéria de linguagem de programação.
+# PSET
+Esse é um PSET passado pelo professor Abrantes Araujo Silva Filho na matéria de linguagem de programação do curso de ciência da computação, ela é inspirada em uma atividade da disciplina MIT6.009:Fundamentals of Programming.
+
+
+## Resposta para as questões do PSET
+
+#### Questão 1: Se você passar essa imagem pelo filtro de inversão, qual seria o output esperado? Justifique sua resposta.
+- largura: 4
+- altura: 1
+- pixels: [29,89,136,200]
+
+ESPERADO:
+- largura: 4
+- altura: 1
+- pixels: [229,166,119,55]
+
+Para a imagem ser invertida em um filtro de inversão, é necessário subtrair o valor de cada pixel de 255.
+Portanto, o pixel [29,89,136,200] se tornaria [229,166,119,55].
+
+#### Questão 2: faça a depuração e, quando terminar, seu código deve conseguir passar em todos os testes do grupo de teste TestInvertida (incluindo especificamente o que você acabou de criar). Execute seu filtro de inversão na imagem test_images/bluegill.png, salve o resultado como uma imagem PNG e salve a imagem.
+
+
---
+++ test.py
@@ -0,0 +1,147 @@
+# IDENTIFICAÇÃO DO ESTUDANTE:
+# Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+# nenhuma linha deste comentário de seu código!
+#
+#    Nome completo: Paulo Rhyan Kuster
+#    Matrícula: 202299971
+#    Turma: CC6N
+#    Email: paulorhyank@gmail.com
+#
+# DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+# Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+# pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+# e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+# Estou ciente de que todo código enviado será verificado automaticamente
+# contra plágio e que caso eu tenha praticado qualquer atividade proibida
+# conforme as normas da disciplina, estou sujeito à penalidades conforme
+# definidas pelo professor da disciplina e/ou instituição.
+#
+
+
+# Imports
+import os
+import pset1
+import unittest
+
+# Diretório
+TEST_DIRECTORY = os.path.dirname(__file__)
+
+
+# Classe para os testes de imagem:
+class TestImagem(unittest.TestCase):
+    def test_carregar(self):
+        resultado = pset1.Imagem.carregar('test_images/centered_pixel.png')
+        esperado = pset1.Imagem(11, 11,
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(resultado, esperado)
+
+
+# Classe para os testes de inversão:
+class TestInvertida(unittest.TestCase):
+    def test_invertida_1(self):
+        im = pset1.Imagem.carregar('test_images/centered_pixel.png')
+        resultado = im.invertida()
+        esperado = pset1.Imagem(11, 11,
+                                [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 0, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+                                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255])
+        self.assertEqual(resultado,  esperado)
+
+    def test_invertida_2(self):
+        imTeste = pset1.Imagem(4, 1, [29, 89, 136, 200])
+        resultado = imTeste.invertida()
+        esperado = pset1.Imagem(4, 1, [226, 166, 119, 55])
+        self.assertEqual(resultado, esperado)
+
+    def test_imagens_invertidas(self):
+        for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=nome_arquivo):
+                arquivo_entrada = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results', '%s_invert.png' % nome_arquivo)
+                resultado = pset1.Imagem.carregar(arquivo_entrada).invertida()
+                esperado = pset1.Imagem.carregar(arquivo_saida)
+                self.assertEqual(resultado,  esperado)
+
+    def test_invertida_peixe(self):
+        arquivo_entrada = os.path.join(TEST_DIRECTORY, 'test_images', 'bluegill.png')  # Caminho do peixe
+        arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results', 'bluegill_invertida.png')  # Caminho do resultado do peixe invertido
+
+        imagem = pset1.Imagem.carregar(arquivo_entrada  ) # Carrega o peixe
+        imagem_invertida = imagem.invertida()  # Inverte o peixe
+        imagem_invertida.salvar(arquivo_saida) # Salva o peixe invertido
+
+
+        imagem_carregada = pset1.Imagem.carregar(arquivo_saida) # Carrega o peixe invertido
+        self.assertEqual(imagem_invertida,imagem_carregada) # Compara o peixe invertido com o peixe invertido carregado
+
+
+# Classe para os testes dos filtros:
+class TestFiltros(unittest.TestCase):
+    def test_borrada(self):
+        for tamanho_kernel in (1, 3, 7):
+            for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=tamanho_kernel, f=nome_arquivo):
+                    arquivo_entrada = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                    arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results',
+                                                 '%s_blur_%02d.png' % (nome_arquivo, tamanho_kernel))
+                    imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                    imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                        imagem_entrada.pixels)
+                    resultado = imagem_entrada.borrada(tamanho_kernel)
+                    esperado = pset1.Imagem.carregar(arquivo_saida)
+                    self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                     "Cuidado para não modificar a imagem original!")
+                    self.assertEqual(resultado,  esperado)
+
+    def test_focada(self):
+        for tamanho_kernel in (1, 3, 9):
+            for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+                with self.subTest(k=tamanho_kernel, f=nome_arquivo):
+                    arquivo_entrada = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                    arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results',
+                                                 '%s_sharp_%02d.png' % (nome_arquivo, tamanho_kernel))
+                    imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                    imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                        imagem_entrada.pixels)
+                    resultado = imagem_entrada.focada(tamanho_kernel)
+                    esperado = pset1.Imagem.carregar(arquivo_saida)
+                    self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                     "Cuidado para não modificar a imagem original!")
+                    self.assertEqual(resultado,  esperado)
+
+    def test_bordas(self):
+        for nome_arquivo in ('mushroom', 'twocats', 'chess'):
+            with self.subTest(f=nome_arquivo):
+                arquivo_entrada = os.path.join(TEST_DIRECTORY, 'test_images', '%s.png' % nome_arquivo)
+                arquivo_saida = os.path.join(TEST_DIRECTORY, 'test_results', '%s_edges.png' % nome_arquivo)
+                imagem_entrada = pset1.Imagem.carregar(arquivo_entrada)
+                imagem_entrada_copia = pset1.Imagem(imagem_entrada.largura, imagem_entrada.altura,
+                                                    imagem_entrada.pixels)
+                resultado = imagem_entrada.bordas()
+                esperado = pset1.Imagem.carregar(arquivo_saida)
+                self.assertEqual(imagem_entrada, imagem_entrada_copia,
+                                 "Cuidado para não modificar a imagem original!")
+                self.assertEqual(resultado,  esperado)
+
+
+if __name__ == '__main__':
+    res = unittest.main(verbosity=3, exit=False)
+
